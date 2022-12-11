43aac7a78052d6e1e9cf7c459a882f667e3d820c
SchokiCoder <afschoknecht@gmail.com>
2022-12-11 17:31:10 +0100

    change license

--- smng_lib/src/lang.rs
+++ smng_lib/src/lang.rs
@@ -1,22 +1,23 @@
 /*
-	"SchokiManager" in short "smng"
-	Copyright (C) 2021 - 2022	Andy Frank Schoknecht
+ * SchokiManager
+ * Copyright (C) 2021 - 2022  Andy Frank Schoknecht
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not see
+ * <https://www.gnu.org/licenses/old-licenses/gpl-2.0.html>.
+ */
 
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
-
-const GNU_WEBSITE: &str = "https://www.gnu.org/licenses/";
+const GNU_WEBSITE: &str = "https://www.gnu.org/licenses/old-licenses/gpl-2.0.html";
 
 i18n_codegen::i18n!("../lang");
 
